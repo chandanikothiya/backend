c5b1d43b702128fed10a9ec207c06bd6d7b8c672
chandanikothiya <kothiyachandani34@gmail.com>
2026-02-10 20:25:22 +0530

    amazon_bill_pdf_tution

--- src/service/pdfmaker.js
+++ src/service/pdfmaker.js
@@ -16,182 +16,109 @@ const makepdf = async () => {
     const docDefinition = {
         content: [
             {
-                
+                alignment: 'justify',
                 columns: [
                     {
-                        image: './public/amazon.png',
-                        width: 100
+                        image: './public/images/amazone.png',
+                        width: 180,
+                        heigth: 50
                     },
                     {
-                        text: 'Tax Invoice/Bill of Supply/Cash Memo (Original for Recipient)'
+                        stack: [
+                            { text: 'Tax Invoice/Bill of Supply/Cash Memo', bold: true, alignment: 'right' },
+                            { text: '(Original for Recipient)', alignment: 'right' }
+                        ],
                     }
-                ]
+                ],
+                margin: [0, 0, 0, 20]
             },
-            '\n\n\n',
             {
-                style: 'items',
+                alignment: 'justify',
                 columns: [
-                    [
-                        { text: 'Sold By :' },
-                        { text: 'SUPER MARKETING ' },
-                        {
-                            text:
-                                `*Super Marketinrg Agency, Plot No. 613, Sumer
-                            Nagar Main, Nea, r Gagan Bharti School,
-                            Mansarovar Jaipur - 302020
-                            JAIPUR, RAJASTHAN, 302020
-                            IN`
-                        }
-                    ],
-                    [
-                        { text: 'Billing Address : :' },
-                        {
-                            text:
-                                `Ankitkumar Navinbhai dharsandiya
-                            A1 402, Opera palm, Kholvad Road
-                            SURAT, GUJARAT, 394185
-                            IN`
-                        },
-                        { text: 'State/UT Code: 24' },
-                    ]
-                ]
-            },
-            '\n',
-            {
-                columns: [
-                    [
-                        { text: 'PAN No: EPSPK8990C:' },
-                        { text: 'GST Registration No: 08EPSPK8990C1Z5' }
-                    ],
-                    [
-                        { text: 'Shipping Address :' },
-                        {
-                            text:
-                                `Ankitkumar Navinbhai dharsandiya
-                            Ankitkumar Navinbhai dharsandiya
-                            A1 402, Opera palm, Kholvad Road
-                            SURAT, GUJARAT, 394185
-                            IN`
-                        },
-                        { text: 'State/UT Code: 24' },
-                        { text: 'Place of supply: GUJARAT ' },
-                        { text: 'Place of delivery: GUJARAT' },
-                    ]
-                ]
-            },
-            '\n',
-            {
-                columns: [
-                    [
-                        { text: 'Order Number: 405-0419869-0333924' },
-                        { text: 'Order Date: 28.01.2025 ' }
-                    ],
-                    [   
-                        { text: 'Invoice Number : IN-803',style:'right' },
-                        { text: 'Invoice Details : RJ-315637153-2425',style:'right' },
-                        { text: 'Invoice Date : 28.01.2025',style:'right' }
-                    ]
-                ]
-            },
-            '\n\n\n',
-            {
-                style: 'tableExample',
-                table: {
-                    body: [
-                        ['Sl.No', 'Description', 'Unit Price', 'Qty', 'Net Amoun', 'Tax Rate', 'Tax Type', 'Tax Amoun', 'Total Amount'],
-                        ['1', `Google Review NFC Card Along with QR Code | Tap or Scan for
-                                Reviews/Feedback | PVC Card with UV Print | Pre-Printed Multicolored
-                                | B0CYD64SY3 ( GRC1001 )
-                                HSN:5201`,
-                            '₹117.80', '2', '₹235.60', '18%', 'IGST', '₹42.40', '278.00'],
-                        ['', 'Shipping Charges', '₹33.90', '', '₹67.80', '18%', 'IGST', '₹12.20', '₹80.00'],
-                        [
-                            {
-                                colSpan: 7,
-                                text: 'TOTAL:',
-                                border: [true, true, true, true],
-                            },
-                            {}, {}, {}, {}, {}, {},
-                            {
-                                text: '₹54.60'
-                            },
-                            {
-                                text: '₹358.00'
-                            },
+                    {
+                        stack: [
+                            { text: 'Sold By :', bold: true },
+                            { text: 'SUPER MARKETING' },
+                            { text: '* Super Marketinrg Agency, Plot No. 613, Sumer' },
+                            { text: 'Nagar Main, Nea, r Gagan Bharti School,r' },
+                            { text: 'Mansarovar Jaipur - 302020' },
+                            { text: 'JAIPUR, RAJASTHAN, 302020' },
+                            { text: 'IN' }
                         ],
-                        [
+                    },
+                    {
+                        stack: [
+                            { text: 'Billing Address :', bold: true, alignment: 'right' },
+                            { text: 'Ankitkumar Navinbhai dharsandiya', alignment: 'right' },
+                            { text: 'A1 402, Opera palm, Kholvad Road', alignment: 'right' },
+                            { text: 'SURAT, GUJARAT, 394185', alignment: 'right' },
+                            { text: 'IN', alignment: 'right' },
                             {
-                                colSpan: 9,
-                                text: `Amount in Words:
-                                        Three Hundred Fifty-eight only`,
-                                border: [true, true, true, true],
-                            },
-                            {}, {}, {}, {}, {}, {}, {}, {}
-                        ],
-                        [
-                            {
-                                colSpan: 9,
                                 text: [
-                                    'For SUPER MARKETING:\n',
-                                    { text: 'Authorized Signatory', bold: true }
-                                ],
-                                alignment: 'right',
-                                border: [true, true, true, true],
-                            },
-                            {}, {}, {}, {}, {}, {}, {}, {}
-                        ]
-                    ]
-                }
+                                    { text: 'State/UT Code:', bold: true },
+                                    { text: 24 }
+                                ], alignment: 'right'
+                            }
+                        ],
+                    }
+                ],
+                margin: [0, 0, 0, 20]
             },
-            'Whether tax is payable under reverse charge - No\n',
             {
                 style: 'tableExample',
                 table: {
                     body: [
                         [
+                            {text:'SI.NO',fillColor:'#808080'},
+                            {text:'Description',fillColor:'#808080'},
+                            {text:'UNIT PRICE',fillColor:'#808080'},
+                            {text:'Qty',fillColor:'#808080'},
+                            {text:'NET AMOUNT',fillColor:'#808080'},
+                            {text:'TAX RATE',fillColor:'#808080'},
+                            {text:'TAX TYPE',fillColor:'#808080'},
+                            {text:'TAX AMOUNT',fillColor:'#808080'},
+                            {text:'Total AMOUNT',fillColor:'#808080'}
+                        ],
+                        [
+                            {text:'1',rowSpan:2},
                             {
-                                text: 
-                                    'Payment Transaction ID:\n1112wwZD1UzLIjSf7OFM9Vqkv'
+                                text:'Google Review NFC Card Along with QR Code | Tap or Scan for\nReviews/Feedback | PVC Card with UV Print | Pre-Printed Multicolored\n| B0CYD64SY3 ( GRC1001 )\nHSN:5201',
+                                border:[true,true,true,false]
                             },
+                            {text:"₹117.80",border:[true,true,true,false]},
+                            {text:"2",border:[true,true,true,false]},
+                            {text:"₹235.60",border:[true,true,true,false]},
+                            {text:"18%",border:[true,true,true,false]},
+                            {text:"IGST",border:[true,true,true,false]},
+                            {text:"₹42.40",border:[true,true,true,false]},
+                            {text:"₹278.00",border:[true,true,true,false]},
+                        ],
+                         [
+                            {text:""},
                             {
-                                text: 'Date & Time: 28/01/2025, 19:09:00hrs'
+                                text:'Shipping Charges',
+                                border:[true,false,true,true]
                             },
-                            {
-
-                                text: 'Invoice Value:\n 358.00',
-                                rowSpan: 2,
-
-                            },
-                            {
-                                text: 'Mode of Payment:\nAmazonPay'
-                            }
-                        ], [
-                            {
-                                text: 'Payment Transaction ID:\n1112rGnrZzfROO1OaWwcPbJwY',
-                            },
-                            {
-                                text: 'Date & Time: 28/01/2025, 19:09:00 hrs'
-                            },
-                            {},
-                            {
-                                text: 'Mode of Payment: \n GiftCard'
-                            }
+                            {text:"₹33.90",border:[true,false,true,true]},
+                            {text:"",border:[true,false,true,true]},
+                            {text:"₹67.80",border:[true,false,true,true]},
+                            {text:"18%",border:[true,false,true,true]},
+                            {text:"IGST",border:[true,false,true,true]},
+                            {text:"₹12.20",border:[true,false,true,true]},
+                            {text:"₹80.00",border:[true,false,true,true]},
+                        ],
+                        [
+                            {text:'TOTAL:',colSpan:7,alignment:'left'},
+                            '','','','','','',
+                            {text:'₹54.60',fillColor:'#808080'},
+                            {text:'₹358.00',fillColor:'#808080'}
                         ]
+
                     ]
                 }
             }
-           
         ],
-        styles: {
-    header: {
-      fontSize: 22,
-      bold: true
-    },
-    anotherStyle: {
-      italics: true,
-      alignment: 'right'
-    }
-  }
+
     };
 
 
