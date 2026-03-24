615842da861720dfcb4952b85a43fce9431aa4d3
chandanikothiya <kothiyachandani34@gmail.com>
2026-03-24 17:56:21 +0530

    c

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -13,7 +13,8 @@ const storage = multer.diskStorage({
         console.log(error)
     })
     
-    cb(null, filepath) //cb is callback function first argument is for error and second is path,filename
+    // cb(null, filepath) //cb is callback function first argument is for error and second is path,filename
+    cb(null, '\tmp')
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
