263f4ff6a00e1e44cd0db6ef2a23708f5fba46ac
chandanikothiya <kothiyachandani34@gmail.com>
2026-05-02 09:37:26 +0530

    sfsf

---
+++ src/controller/result.controller.js
@@ -0,0 +1,11 @@
+const result = require('../models/result.model')
+
+const addresult = async (req,res) => {
+    try {
+        const checkresult = await res
+
+        
+    } catch (error) {
+        
+    }
+}
---
+++ src/models/result.model.js
@@ -0,0 +1,30 @@
+const { default: mongoose } = require("mongoose");
+
+const resultquizeschema = mongoose.Schema({
+    course_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'courses'
+    },
+    user_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'users'
+    },
+    quiz_id: {
+        type: mongoose.Types.ObjectId,
+        ref: 'quizes'
+    },
+    obtain_marks: {
+        type: Number,
+        require: true
+    },
+    total_marks: {
+        type: Number,
+        require: true
+    }
+}, {
+    timestamps: true,
+    versionKey: false
+})
+
+const result = mongoose.model('quize', resultquizeschema)
+module.exports = result;
