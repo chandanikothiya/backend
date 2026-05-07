7c1811312312000485cb0d05e466ba9d1ec0b1f1
chandanikothiya <kothiyachandani34@gmail.com>
2026-05-07 19:02:00 +0530

    dd

--- src/controller/payment.controller.js
+++ src/controller/payment.controller.js
@@ -17,45 +17,93 @@ const createpayment = async (req, res) => {
                 "payment_method": "paypal"
             },
             "redirect_urls": {
-                "return_url": "http://localhost:5173",
-                "cancel_url": "http://localhost:5173/"
+                "return_url": "http://localhost:8080/api/v1/payment/successpayment",
+                "cancel_url": "http://localhost:8080/api/v1/payment/failedpayment"
             },
             "transactions": [{
                 "item_list": {
                     "items": [{
                         "name": "Red Sox Hat",
                         "sku": "001",
-                        "price": "25.00",
+                        "price": "1.00",
                         "currency": "USD",
                         "quantity": 1
                     }]
                 },
                 "amount": {
                     "currency": "USD",
-                    "total": "25.00"
+                    "total": "1.00"
                 },
                 "description": "Hat for the best team ever"
             }]
         };
 
-        await  paypal.payment.create(
-        create_payment_json,
-        function (error, payment) {
-            if (error) {
-                console.log(error)
-                throw error;
-            } else {
-                console.log(payment)
+        await paypal.payment.create(
+            create_payment_json,
+            function (error, payment) {
+                if (error) {
+                    console.log(error)
+                    throw error;
+                } else {
+                    console.log(payment)
 
-                res.json({data:payment})
-            }
-        });
+                    res.json({ data: payment })
+                }
+            });
 
     } catch (error) {
         console.log(error)
     }
 }
 
+const successpayment = async (req, res) => {
+    try {
+        console.log("req.query", req.query)
+
+        const PayerID = req.query.PayerID;
+        const paymentId = req.query.paymentId;
+
+        const express_checkout_json = {
+            "payer_id": PayerID,
+            "transactions": [{
+                "amount": {
+                    "currency": "USD",
+                    "total": "1.00"
+                },
+                "description": "this is payment description"
+            }]
+        }
+
+        paypal.payment.execute(paymentId, express_checkout_json, function (error, payment) {
+            if (error) {
+                console.log(error)
+                return res.redirect("http://localhost:5173/filed")
+                throw error;
+            } else {
+                const response = JSON.stringify(payment);
+                const parseres = JSON.parse(response)
+
+                console.log(response)
+
+                return res.redirect("http://localhost:5173/success")
+            }
+        })
+
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
+const failedpayment = async (req, res) => {
+    try {
+       return res.redirect("http://localhost:5173/filed")
+    } catch (error) {
+        console.log("error", error)
+    }
+}
+
 module.exports = {
-    createpayment
+    createpayment,
+    successpayment,
+    failedpayment
 }
--- src/routes/api/v1/payment.routes.js
+++ src/routes/api/v1/payment.routes.js
@@ -5,6 +5,12 @@ const router = express.Router();
 //http://localhost:8080/api/v1/payment/getallPayment
 router.get('/createpayment',paymentController.createpayment)
 
+router.get('/successpayment',paymentController.successpayment)
+
+router.get('/failedpayment',paymentController.failedpayment)
+
+
+
 router.get('/getallPayment',(req,res) => {
     res.status(200).json({message:'all Payment fetch'})
 })
